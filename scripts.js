7b62ea99b632f3144df1bb6422a4e8ae824bedd0
Adam Ledet <acl6865@g.rit.edu>
2019-04-22 18:00:54 -0400

    Start of LoL implimentation
    I added the LoL index to the scripts and gave some UI to fit. Currently, clicking 'search' will read the input field and search for that user. It 'works,' but we still have CORS issues, so it will give an error every time 'search' is clicked.

--- scripts.js
+++ scripts.js
@@ -49,8 +49,14 @@ const app = new Vue({
 });*/
 
 
+//Twitch Info
 const twitchURL = "https://api.twitch.tv/kraken";
 const clientID = "4gqmz463yaeixhvuvwuposq1j5maxb";
+
+//League Info
+const leagueURL = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/";
+let summonerName = "RiotSchmick";
+const leagueKey = "?api_key=RGAPI-c90a6ee2-aef5-4bdb-b4fb-834ae9636466";//Key needs to be updated every 24 hours
 
 const app = new Vue({
 	el: '#app',
@@ -75,6 +81,21 @@ const app = new Vue({
 					
 				}
 			});
-		}
+		},
+        getLeagueData: function(){
+            if(document.querySelector("#summonerNameInput").value != "")
+                {
+                    summonerName = document.querySelector("#summonerNameInput").value;
+                }
+            else
+            {
+                summonerName = "RiotSchmick";
+            }
+            url = leagueURL + summonerName + leagueKey;
+            $.getJSON(url, function(data){
+                    console.log(data.name);
+                    console.log(data.profileIconId);
+                })
+        }
 	} // end methods
 });
